fdff5ce44fde4196262b5c26452743d9ba0be952
trevisharp <leonardotrevisansilio@gmail.com>
2026-09-09 23:36:12 -0300

    basic noble gas dispersion

---
+++ Noble/Program.cs
@@ -0,0 +1,155 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+const int N1 = 5000;
+const int N2 = 5000;
+const float T1 = 10f;
+const float T2 = 10f;
+const float m = 0.01f;
+
+const int ConcFrameImpact = 50;
+const int Frames = 50;
+const int SimulTicksPerFrame = 10;
+
+var x = new float[N1 + N2];
+var y = new float[N1 + N2];
+var dx = new float[N1 + N2];
+var dy = new float[N1 + N2];
+
+var wx = 1f;
+var wdx = 0f;
+
+// T \approx m * v^2 / 2 --> v \approx sqrt(2 T / m)
+var v1 = float.Sqrt(2 * T1 / (N1 * m));
+var v2 = float.Sqrt(2 * T2 / (N2 * m));
+
+for (int k = 0; k < N1; k++)
+{
+    x[k] = Random.Shared.NextSingle();
+    y[k] = Random.Shared.NextSingle();
+
+    var theta = MathF.Tau * Random.Shared.NextSingle();
+    dx[k] = v1 * MathF.Cos(theta);
+    dy[k] = v1 * MathF.Sin(theta);
+}
+
+for (int k = 0; k < N2; k++)
+{
+    x[N1 + k] = 1f + Random.Shared.NextSingle();
+    y[N1 + k] = Random.Shared.NextSingle();
+
+    var theta = MathF.Tau * Random.Shared.NextSingle();
+    dx[N1 + k] = v2 * MathF.Cos(theta);
+    dy[N1 + k] = v2 * MathF.Sin(theta);
+}
+
+#region Core
+
+ApplicationConfiguration.Initialize();
+
+var form = new Form {
+    WindowState = FormWindowState.Maximized,
+    FormBorderStyle = FormBorderStyle.None,
+    TransparencyKey = Color.Magenta,
+    ShowInTaskbar = false,
+    TopMost = true
+};
+
+form.KeyDown += (o, e) =>
+{
+    if (e.KeyCode == Keys.Escape)
+        form.Close();
+};
+
+form.Load += (o, e) => Start();
+
+Bitmap bmp = null!;
+var pb = new PictureBox {
+    Dock = DockStyle.Fill
+};
+form.Controls.Add(pb);
+form.Load += (o, e) => 
+    pb.Image = bmp = new Bitmap(pb.Width, pb.Height);
+
+var timer = new Timer {
+    Interval = 20
+};
+form.Load += (o, e) => timer.Start();
+
+var now = DateTime.UtcNow;
+form.Load += (o, e) => now = DateTime.UtcNow;
+
+timer.Tick += (o, e) =>
+{
+    var g = Graphics.FromImage(bmp);
+
+    var newNow = DateTime.UtcNow;
+    var dt = (float)(newNow - now).TotalSeconds / SimulTicksPerFrame;
+    for (int i = 0; i < SimulTicksPerFrame; i++)
+        Simulate(dt);
+    now = newNow;
+    
+    g.Clear(Color.Magenta);
+    Draw(g);
+    pb.Refresh();
+};
+
+Application.Run(form);
+
+#endregion
+
+void Start()
+{
+    
+}
+
+void Simulate(float dt)
+{
+    
+}
+
+void Draw(Graphics g)
+{
+    var size = float.Min(
+        8 * form.Height / 10,
+        4 * form.Width / 10
+    );
+    var rect = new RectangleF(
+        (form.Width - 2 * size) / 2,
+        (form.Height - size) / 2,
+        2 * size, size
+    );
+
+    g.Clear(Color.Black);
+    g.FillRectangle(Brushes.White, rect);
+
+    var array = new int[2 * Frames * Frames];
+    for (int k = 0; k < x.Length; k++)
+    {
+        var i = (int)(Frames * x[k]);
+        var j = (int)(Frames * y[k]);
+        array[i + j * 2 * Frames]++;
+    }
+
+    var a = size / Frames;
+    for (int j = 0; j < Frames; j++)
+        for (int i = 0; i < 2 * Frames; i++)
+        {
+            var index = i + j * 2 * Frames;
+            var concentration = array[index];
+            var level = int.Max(0, 255 - ConcFrameImpact * concentration);
+
+            var color = i < wx * Frames ?
+                Color.FromArgb(255, level, level) :
+                Color.FromArgb(level, level, 255);
+            var brush = new SolidBrush(color);
+            
+            g.FillRectangle(
+                brush,
+                rect.X + i * a,
+                rect.Y + j * a,
+                a, a
+            );
+        }
+}
